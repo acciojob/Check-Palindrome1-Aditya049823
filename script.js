6717b2b3495085d7d9afdf9ea7826dc4a5eab870
Aditya Harde <110459473+Aditya049823@users.noreply.github.com>
2023-06-14 18:38:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // complete the given function
 
 function palindrome(str){
+	let lower=str.toLowerCase();
 	let str1="";
-	for(let i=str.length-1;i>=0;i--)
+	for(let i=lower.length-1;i>=0;i--)
 		{
-			str1+=str.charAt(i);
+			str1+=lower.charAt(i);
 		}
-	if(str==str1)
+	if(lower==str1)
 	{
 		return true;
 	}
